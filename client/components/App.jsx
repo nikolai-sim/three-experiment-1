29985295051fc0c0d949bead9694d730eb7f35a6
student <student@eda-bc-02.localdomain>
2022-02-01 18:04:08 +1300

    add canvas to project

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from 'three'
+
+const Scene = () => {
+  return ( 
+    <Canvas>
+      <pointLight position={[10, 10, 10]}/>
+      {/* <PerspectiveCamera> */}
+      <mesh>
+        <planeBufferGeometry args={[3, 5]}/>
+        <meshStandardMaterial color='lightblue'/>
+      </mesh>
+    </Canvas>
+  )
+}
 
 function App () {  
   return (
-    <h1>React development has begun!</h1>
+    <Scene></Scene>
   )
 }
 
