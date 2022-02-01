7d8518d05226ad7ea39b268f129b7b3d67aadfe7
nikolai-sim <nikolai_sim@hotmail.com>
2022-02-01 20:56:17 +1300

    add mesh and clickHandle

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera } from 'three'
+import { Stars, softShadows, MeshWobbleMaterial, OrbitControls} from '@react-three/drei'
+
+
 
 const Scene = () => {
+
   return ( 
     <Canvas>
-      <pointLight position={[10, 10, 10]}/>
+      <ambientLight intensity={0.5}/>
+      <spotLight position={[50, 50, 50]}/>
+      <Stars/>
+      {/* <pointLight position={[10, 10, 10]}/> */}
       {/* <PerspectiveCamera> */}
       <mesh>
-        <planeBufferGeometry args={[3, 5]}/>
-        <meshStandardMaterial color='lightblue'/>
+        {/* <planeBufferGeometry args={[3, 5]}/>
+        <meshStandardMaterial color='lightblue'/> */}
+        <OrbitControls />
+        <Box/>
+        
       </mesh>
+      
     </Canvas>
+  )
+}
+
+const Box = () => {
+
+  const colorArr = ['white', 'red', 'orange' , 'yellow' , 'green', 'blue' , 'indigo' , 'violet', 'white']
+
+  
+  const [color , setColor] = useState('white')
+
+  const clickHandle = () => {
+    const i = Math.floor(Math.random() * 8)
+    setColor(colorArr[i])
+  }
+  
+
+  return (
+    <>
+    <mesh onClick={clickHandle}>
+      <torusBufferGeometry attach='geometry' color ='0xFF6347'  wireframe='true'/>
+      <meshBasicMaterial color ={color} wireframe='false'/>
+      
+    </mesh>
+    </>
+    
   )
 }
 
