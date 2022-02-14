fe4db43d82b0eb9d3b5df24ca25f78c99c98af1b
nikolai-sim <89240062+nikolai-sim@users.noreply.github.com>
2022-02-14 17:23:52 +1300

    add keyboard event handlers to move object through space

--- client/components/Scene.jsx
+++ client/components/Scene.jsx
@@ -3,18 +3,39 @@ import { Canvas } from '@react-three/fiber'
 import { Stars, softShadows, MeshWobbleMaterial, OrbitControls, Cloud} from '@react-three/drei'
 import Box from './Box'
 import Board from './Board'
+import KeyboardEventHandler from 'react-keyboard-event-handler'
 
 
 
 
 const Scene = () => {
+  const [x , setX ] = useState(0)
+  const [y , setY ] = useState(0)
+  const [z , setZ ] = useState(0)
+
+  const handleKey = (key , e) => {
+    switch (key) {
+      case 'left' : setX((x - 1))
+      break
+      case 'up' : setY(( y + 1 ))
+      break
+      case 'right' : setX(( x + 1 ))
+      break
+      case 'down' : setY(( y - 1))
+
+    }
+  }
 
   return ( 
+    <>
+    <KeyboardEventHandler
+      handleKeys={['left' , 'up' , 'right' , 'down' ]}
+      onKeyEvent={handleKey} />
     <Canvas>
       <ambientLight intensity={0.5}/>
       <spotLight position={[50, 50, 50]}/>
       <Stars/>
-      <mesh>
+      <mesh position = {[x , y , z]}>
         <OrbitControls />
         <Box/>
       </mesh>
@@ -23,6 +44,7 @@ const Scene = () => {
        <Board/>
       </mesh>
     </Canvas>
+    </>
   )
 }
 
